8cd2082ce27c7739d57e4e455f4a4e9b19c43dc7
Olim Sadullaev <olimxonnutfullaevich@gmail.com>
2024-08-08 14:39:33 +0500

    Admin page

--- VideoPlatform/VideoPlatform/Controllers/VideoController.cs
+++ VideoPlatform/VideoPlatform/Controllers/VideoController.cs
@@ -1,23 +1,131 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using VideoPlatform.DataDB;
 using VideoPlatform.Models;
 
 namespace VideoPlatform.Controllers
 {
     public class VideoController : Controller
     {
-        private static readonly List<VideoModel> videos = new List<VideoModel>
+        private readonly ApplicationDbContext context;
+        public VideoController(ApplicationDbContext context)
+        {
+            this.context = context;   
+        }
+
+        /*private static readonly List<VideoModel> videos = new List<VideoModel>
         {
         new VideoModel { Id = 1, Title = "Lesson 1", Description = "Description of Lesson 1", Url = "https://www.youtube.com/embed/videoid1" },
         new VideoModel { Id = 2, Title = "Lesson 2", Description = "Description of Lesson 2", Url = "https://www.youtube.com/embed/videoid2" }
-        // Add more videos here
-    };
+        };*/
+
         public IActionResult Index()
         {
+            var videos = context.Videos.ToList();
             return View(videos);
         }
 
-        public IActionResult Details(int id) 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(VideoModel video)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(video);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(video);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var video = await context.Videos.FindAsync(id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+            return View(video);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, VideoModel video)
+        {
+            if (id != video.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    context.Update(video);
+                    await context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VideoExists(video.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(video);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var video = await context.Videos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+
+            return View(video);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var video = await context.Videos.FindAsync(id);
+            context.Videos.Remove(video);
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VideoExists(int id)
+        {
+            return context.Videos.Any(e => e.Id == id);
+        }
+
+
+
+        /*public IActionResult Details(int id) 
         { 
             var video = videos.Find(v => v.Id == id);
             if (video == null)
@@ -25,6 +133,6 @@ namespace VideoPlatform.Controllers
                 return NotFound();
             }
             return View(video);
-        }
+        }*/
     }
 }
---
+++ VideoPlatform/VideoPlatform/Views/Admin/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VideoPlatform.Views.Admin
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
